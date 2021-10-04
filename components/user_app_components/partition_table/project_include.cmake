64e5cc84bfb91f1992f9fed6f6a67ec5b98d4331
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2021-10-04 13:56:45 +0530

    user_app_components: Add components required for user app build

---
+++ components/user_app_components/partition_table/project_include.cmake
@@ -0,0 +1,6 @@
+# partition_table_get_partition_info
+#
+# Get information about a partition from the partition table
+function(partition_table_get_partition_info result get_part_info_args part_info)
+    message(STATUS "Cannot access partition table from user_app")
+endfunction()
