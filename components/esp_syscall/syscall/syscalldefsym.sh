f1a62e96c4c6b7740ba759df73dcc07c36bc5d0c
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2022-04-22 11:29:47 +0530

    Change defsym passing mechanism to user app
    Define all usr_* symbols using -u linker flag

--- components/esp_syscall/syscall/syscalldefsym.sh
+++ components/esp_syscall/syscall/syscalldefsym.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-syscall_tbl="$1"
-override_tbl="$2"
+# syscalldefsym.sh generates the list of defsyms required for user app.
+# This script helps to remove usr_ prefix for all the specified symbols.
+
+# syscall_tbl is the table which actually stores system call entries.
+# Where as override_tbl is used to map one symbol to another.
+# Hence, we generate defsym entries for both the tables.
+syscall_tbl="$1"    # Path to syscall.tbl file
+override_tbl="$2"   # Path to override.tbl file
 
 syscall_defsym_list=$(grep -E "^[0-9A-Fa-fXx]+[[:space:]]" "$syscall_tbl" | sort -n | (
 	while read nr abi name entry ; do
         if [ "$abi" = "common" ];
         then
-            list="${list}-Wl,--wrap=${name} -Wl,--defsym=__wrap_${name}=usr_${name} "
+            list="${list}-u usr_${name} -Wl,--wrap=${name} -Wl,--defsym=__wrap_${name}=usr_${name} "
         fi
 	done
     printf "%s" "${list}"
