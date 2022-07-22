ded659f7c7ffe165df18bb2a22770151c8eeff6c
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2022-07-22 16:20:04 +0530

    esp_priv_build_utils: Do not pass CUSTOM_SYSCALL_TBL variable to user app build

--- components/shared/esp_priv_build_utils/project_include.cmake
+++ components/shared/esp_priv_build_utils/project_include.cmake
@@ -80,7 +80,6 @@ externalproject_add(user_app
         -DSDKCONFIG=${sdkconfig} -DPROTECTED_CONFIG_DIR=${config_dir} \
         -DIDF_PATH=${idf_path} -DIDF_TARGET=${idf_target} \
         -DPYTHON_DEPS_CHECKED=1 -DPYTHON=${python} \
-        -DCUSTOM_SYSCALL_TBL=${CUSTOM_SYSCALL_TBL} \
         -DCCACHE_ENABLE=${CCACHE_ENABLE} \
         -DDEFSYM_LIST=\"$DEFSYM\" \
         ${extra_cmake_args} \
